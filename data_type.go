e5acb95d31600c0ac7fd512fd390a7fd96a19104
“rahulislamitibd” <itibdd@gmail.com>
2021-02-12 09:12:00 +0600

    data_type.go

---
+++ data_type.go
@@ -0,0 +1,23 @@
+package main 
+import "fmt"
+func main(){
+/*
+32.2
+23
+numaric type 
+'123rahul'
+string type 
+ture and false 
+boolean type
+(123, 'tomar', '123amaer', 12.5)
+Derived type
+*/
+var a string= "name is Rahul"
+fmt.Println(a)
+var b int=1254
+fmt.Println(b)
+var c float32=3.55
+fmt.Println(c)
+var d rune='o'
+fmt.Println(d)
+}
